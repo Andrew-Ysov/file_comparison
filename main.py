ae8be695a0dfb9bc8eb692d94902ad253017cecb
Усов Андрей <destracthioner@gmail.com>
2025-02-10 10:43:55 +0300

    Update main.py
    отформатирован внешний вид кода

--- main.py
+++ main.py
@@ -1,51 +1,51 @@
 import os
 import shutil
-from paths import main_disk, backup
+from paths import MAIN_DISK, BACKUP
 
 
-disk = main_disk
-flash = backup
+DISK = MAIN_DISK
+FLASH = BACKUP
 
-n = len(disk)
-m = len(flash)
+n = len(DISK)
+m = len(BACKUP)
 
-dirs = set()
-files = set()
+DIRS = set()
+FILES = set()
 
-for path, dirs_on_disk, files_on_disk in os.walk(disk):
-
+# Собирает пути до всех файлов и папок
+for path, dirs_on_disk, files_on_disk in os.walk(DISK):
     for directory in dirs_on_disk:
-        dirs.add(path[n:] + '/' + directory)
+        DIRS.add(path[n:] + '/' + directory)
 
     for file in files_on_disk:
-        files.add(path[n:] + '/' + file)
+        FILES.add(path[n:] + '/' + file)
 
-
-for path, dirs_on_flash, files_on_flash in os.walk(flash):
-    
+# Оставляет в DIRS, FILES только уникальные для DISK пути, которых нет в FLASH
+for path, dirs_on_flash, files_on_flash in os.walk(FLASH):
     for directory in dirs_on_flash:
         new_path = path[m:] + '/' + directory
 
-        if new_path in dirs:
-            dirs.remove(new_path)
+        if new_path in DIRS:
+            DIRS.remove(new_path)
         else:
             shutil.rmtree(path + '/' + directory)
     
     for file in files_on_flash:
-
         new_path = path[m:] + '/' + file
         
-        if new_path in files:
-            files.remove(new_path)
+        if new_path in FILES:
+            FILES.remove(new_path)
         elif os.path.isfile(path + '/' + file):
             os.remove(path + '/' + file)
 
-for directory in dirs:
-    new_path = flash + directory
+
+for directory in DIRS:
+    new_path = FLASH + directory
     if not os.path.isdir(new_path):
         os.makedirs(new_path)
 
+
 for file in files:
-    old_path = disk + file
-    new_path = flash + file
+    old_path = DISK + file
+    new_path = FLASH + file
     shutil.copy(old_path, new_path)
