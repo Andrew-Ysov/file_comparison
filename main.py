a7e7d018cdd531a04cc7a5797cc16ae96c873587
Усов Андрей <destracthioner@gmail.com>
2025-02-20 14:46:11 +0300

    функоционал помещён в отдельную функцию

--- main.py
+++ main.py
@@ -13,40 +13,45 @@ m = len(BACKUP)
 dirs = set()
 files = set()
 
-# Собирает пути до всех файлов и папок
-for path, dirs_on_disk, files_on_disk in os.walk(DISK):
-    for directory in dirs_on_disk:
-        dirs.add(path[n:] + '/' + directory)
 
-    for file in files_on_disk:
-        files.add(path[n:] + '/' + file)
+def main(dirs, files, m, n, DISK, FLASH):
+    # Собирает пути до всех файлов и папок
+    for path, dirs_on_disk, files_on_disk in os.walk(DISK):
+        for directory in dirs_on_disk:
+            dirs.add(path[n:] + '/' + directory)
 
-# Оставляет в dirs, files только уникальные для DISK пути, которых нет в FLASH
-for path, dirs_on_flash, files_on_flash in os.walk(FLASH):
-    for directory in dirs_on_flash:
-        new_path = path[m:] + '/' + directory
+        for file in files_on_disk:
+            files.add(path[n:] + '/' + file)
 
-        if new_path in dirs:
-            dirs.remove(new_path)
-        else:
-            shutil.rmtree(path + '/' + directory)
-    
-    for file in files_on_flash:
-        new_path = path[m:] + '/' + file
+    # Оставляет в dirs, files только уникальные для DISK пути, которых нет в FLASH
+    for path, dirs_on_flash, files_on_flash in os.walk(FLASH):
+        for directory in dirs_on_flash:
+            new_path = path[m:] + '/' + directory
+
+            if new_path in dirs:
+                dirs.remove(new_path)
+            else:
+                shutil.rmtree(path + '/' + directory)
         
-        if new_path in files:
-            files.remove(new_path)
-        elif os.path.isfile(path + '/' + file):
-            os.remove(path + '/' + file)
+        for file in files_on_flash:
+            new_path = path[m:] + '/' + file
+            
+            if new_path in files:
+                files.remove(new_path)
+            elif os.path.isfile(path + '/' + file):
+                os.remove(path + '/' + file)
 
 
-for directory in dirs:
-    new_path = FLASH + directory
-    if not os.path.isdir(new_path):
-        os.makedirs(new_path)
+    for directory in dirs:
+        new_path = FLASH + directory
+        if not os.path.isdir(new_path):
+            os.makedirs(new_path)
 
 
-for file in files:
-    old_path = DISK + file
-    new_path = FLASH + file
-    shutil.copy(old_path, new_path)
+    for file in files:
+        old_path = DISK + file
+        new_path = FLASH + file
+        shutil.copy(old_path, new_path)
+
+if __name__ == '__main__':
+    main(dirs, files, m, n, DISK, FLASH)
