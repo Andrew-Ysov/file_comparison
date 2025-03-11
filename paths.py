76362246f5dbb64c0970a63f2e6c1fdbbf323336
Усов Андрей <destracthioner@gmail.com>
2025-03-11 14:09:34 +0300

    deleted excessive functionality, fixed typo

--- paths.py
+++ paths.py
@@ -1,9 +1,4 @@
 """Variables for path to disk and it's backup."""
 
-def main():
-  """Specify your own paths."""
-  MAIN_DISK = 'D:/Главная папка'
-  BACKUP = 'E:/Главная папка'
-
-if __name__ == '__main__':
-  main()
+MAIN_DISK = 'D:/Главная папка'
+BACKUP = 'E:/Главная папка'
